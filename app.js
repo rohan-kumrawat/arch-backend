d0373a85d91923e0edc5a426cb8f054e92674317
rohan-kumrawat <rohanrkmail@gmail.com>
2024-11-26 23:40:26 +0530

    Add public features till now

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const sliderRoutes = require('./routes/sliderRoutes');
 const projectRoutes = require('./routes/projectRoutes');
 const reviewRoutes = require('./routes/reviewRoutes');
 const publicRoutes = require('./routes/publicRoutes');
+//const clientRoutes = require('./routes/clientRoutes');
 
 const app = express();
 connectDB();
@@ -19,6 +20,7 @@ app.use('/api/slider', sliderRoutes);
 app.use('/api/project', projectRoutes);
 app.use('/api/review', reviewRoutes);
 app.use('/api/public', publicRoutes);
+//app.use('/api/client', clientRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
